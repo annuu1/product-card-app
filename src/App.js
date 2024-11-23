7aa4dae614eb7f68a651fcdf0f4fce2be3109668
annuu1 <mannusng876@gmail.com>
2024-11-23 17:51:10 +0530

    card creation done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+// import { useState } from 'react';
+import React, {useState} from 'react';
+import ProductCard from './ProductCard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+        const [products, setProducts] = useState([
+            { id: 1, name: 'Product 1', price: 100, isFavorited: true, description: 'This is a great product.', isExpanded: true},
+            { id: 2, name: 'Product 2', price: 200, isFavorited: false, description: 'This is another great product.', isExpanded: true },
+            { id: 3, name: 'Product 2', price: 200, isFavorited: false, description: 'This is another great product.',  isExpanded: false},
+            { id: 4, name: 'Product 2', price: 200, isFavorited: false, description: 'This is another great product.',  isExpanded: true},
+            { id: 5, name: 'Product 2', price: 200, isFavorited: true, description: 'This is another great product.',  isExpanded: true},
+        ]);
+
+        function toggleFav(id){
+            let newProducts =products.map((product)=>{
+                if(product.id === id){
+                    return { ...product, isFavorited: !product.isFavorited };
+                }
+                return product;
+            })
+            setProducts(newProducts);
+        }
+
+        function toggleFeatures(id){
+            let newProducts = products.map((product) =>{
+                if(product.id === id){
+                    return { ...product, isExpanded : !product.isExpanded}
+            }
+            return product;
+         })
+         setProducts(newProducts);
+        }
+
+    return (
+        <div className="App">
+            <h1>Product Card Application</h1>
+            <button onClick={() => toggleFav(4)}>Count : {products[0].isFavorited ? products[0].price : "0"}</button>
+            <div className="product-cards">
+                {
+                    products.map((product) => {
+                        return <ProductCard key={product.id} {...product} toggleFav={toggleFav} toggleFeatures={toggleFeatures} />
+                    })
+                }
+            </div>
+            
+        </div>
+    );
+};
 
 export default App;
---
+++ src/ProductCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./ProductCard.css";
+
+const ProductCard = (product) => {
+
+  return (
+    <div className={`product-card ${product.isFavorited? "favorited": ""}`}>
+
+      <img src="https://m.media-amazon.com/images/I/71ZAkMvUuaL._SL1500_.jpg" alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>Price: ${product.price}</p>
+      <p>{product.description}</p>
+      <div>
+        <button onClick={() => product.toggleFav(product.id)} className="fav-icon">{product.isFavorited ? "❤️" : "🤍"}</button>
+        <button onClick={()=> product.toggleFeatures(product.id)}>
+            {product.isExpanded ? 'Hide Features' : 'Show Features'}
+        </button>
+        {
+          product.isExpanded && (
+            <ul>
+              <li>Feature 1</li>
+              <li>Feature 2</li>
+              <li>Feature 3</li>
+            </ul>
+          )
+        }
+      </div>  
+    </div>
+  );
+};
+
+export default ProductCard;
